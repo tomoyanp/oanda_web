15d6994d4898b7dd04d244c6d40cdbb9df8db065
tomoyan <tomoyanpy@gmail.com>
2017-10-29 16:50:28 +0900

    modi

--- static/js/drawline.js
+++ static/js/drawline.js
@@ -1,22 +1,36 @@
-draw();
+createsvg();
 
 
-var sample = [
-  ["2017-10-23 12:00:00", "143.0"],
-  ["2017-10-23 12:01:00", "143.1"],
-  ["2017-10-23 12:02:00", "143.2"],
-  ["2017-10-23 12:03:00", "143.3"],
-  ["2017-10-23 12:04:00", "143.4"],
-  ["2017-10-23 12:05:00", "143.5"],
-  ["2017-10-23 12:06:00", "143.6"]
-]
 
-function draw() {
-	var h = 300;
-	var w = 1000;
-	var svg = d3.select("#result")
-				.append("svg")
-				.attr("width", w)
-				.attr("height", h);
+function createsvg() {
+  var svg = d3.select("#chart").append("svg")
+    .attr({
+      width: 640,
+      height: 480,
+    });
 
-}
+  var c1 = [
+    "2017-10-23 12:00:00",
+    "2017-10-23 12:01:00",
+    "2017-10-23 12:02:00",
+    "2017-10-23 12:03:00",
+    "2017-10-23 12:04:00",
+    "2017-10-23 12:05:00",
+    "2017-10-23 12:06:00"
+  ]
+
+  var c2 = [143.0, 143.1, 143.2, 143.3, 143.4, 143.5, 143.6];
+
+  var carray = [c1, c2]
+
+  var line = d3.svg.line()
+    .x(function(d) { return d[0];})
+    .y(function(d) { return d[1];});
+
+  var path = svg.append("path")
+    .attr({
+      "d": line(carray),
+      "stroke": "lightgreen",
+      "stroke-width": 5,
+    });
+};
